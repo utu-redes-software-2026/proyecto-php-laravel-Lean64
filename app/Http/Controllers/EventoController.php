3bde9a02fb7023e3fddbf25c3597c831a6c6e70c
Lean64 <leandrofrancia18@gmail.com>
2026-04-15 23:17:41 -0300

    Siguiente commit - controlador de eventos

---
+++ app/Http/Controllers/EventoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventoController extends Controller
+{
+    public function index()
+    {
+        return "Listado de eventos desde el controlador";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
 
+Route::get('/eventos', [EventoController::class, 'index']);
 Route::get('/', function () {
     return view('bienvenido');
 });
